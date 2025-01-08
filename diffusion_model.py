45b63a3ab1e81c18b0b81cc79bb871dc8894083e
elishartung <elishartung@gmail.com>
2025-01-08 21:06:29 +0000

    Remove extra statements and arguments

--- diffusion_model.py
+++ diffusion_model.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 D = 100 
 Lx = 300
 
@@ -12,9 +11,6 @@ Lx = 300
 dx=0.5
 x= np.arange(start=0,stop=Lx,step=dx)
 nx=len(x)
-
-
-whos
 
 
 C = np.zeros_like(x)
@@ -36,9 +32,6 @@ nt = 5000
 dt =  0.5 * dx**2 / D
 
 
-dt
-
-
 for t in range(0, nt):
     C+= D * dt / dx**2 * (np.roll(C, -1) - 2*C + np.roll(C,1))
     C[0] = C_left
@@ -50,14 +43,4 @@ plt.plot(x,C,"b")
 plt.xlabel("x")
 plt.ylabel("C")
 plt.title("Final Concentration Profile")
-
-
-z =np.arange(5)
-z
-
-
-np.roll(z, -1)
-
-
-np.roll(z,1)
 
