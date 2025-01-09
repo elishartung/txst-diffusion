a6cb298da8b31f8f063d10d5abc36aa941ede3aa
elishartung <elishartung@gmail.com>
2025-01-09 17:40:53 +0000

    Set up sample main program

--- diffusion_model.py
+++ diffusion_model.py
@@ -26,27 +26,37 @@ def plot_profile(grid, concentration, color="r", title="COncentraiton Profiile")
     plt.title(title)
 
 def solve_ld_diffusion(concentration, grid_spacing=1.0, time_step=1.0, diffusivity=1.0):
-    centered_difference= (np.roll(concentration, -1) - 2*concentration + np.roll(concentration,1)
+    centered_difference= (np.roll(concentration, -1) - 2*concentration + np.roll(concentration,1))
     concentration[1:-1] += diffusivity + time_step / grid_spacing**2 * centered_difference[1:-1]
 
 
-def diffusion_model():
+def diffusion_model(plot=False, domain_size=300, n_time_steps=5000):
     D = 100 
-    Lx = 300
+    Lx = domain_size
     dx=0.5
     C_left = 500
     C_right= 0
-    nt = 5000
+    nt = n_time_steps
 
 
     x, nx = make_grid(0, Lx,dx)
     dt =  calculate_time_step(dx, D)
     C=set_initial_profile(nx, boundary_left=C_left, boundary_right=C_right)
 
-    plot_profile(x,C, title="Initial concentration profile")
+    if plot is True:
+        plot_profile(x,C, title="Initial concentration profile")
 
+    if plot is False:
+        print("Time= 0\n, C")
     for t in range(0, nt):
         solve_ld_diffusion(C,dx, dt, D)
-
-    plot_profile(x,C, color="b", title="Final concentration profile")
+        if plot is False:
+                print(f"Time = {t*dt}\n", C) 
+    
+    if plot is True:
+        plot_profile(x,C, color="b", title="Final concentration profile")
                       
+
+if __name__ == "__main__": 
+    print("Diffusion model")
+    diffusion_model(plot=False, domain_size=5, n_time_steps=5)
